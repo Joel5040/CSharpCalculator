573df5a7d07ae3ced15a16318143f3b722523a01
Joel5040 <joelthompson5040>
2025-08-20 21:06:28 +0100

    feat: add multiplication function

--- Program.cs
+++ Program.cs
@@ -4,10 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Addition: 5 + 3 = " + Add(5, 3));
-        Console.WriteLine("Subtraction: 10 - 4 = " + Subtract(10, 4));
+        Console.WriteLine("Multiplication: 6 * 7 = " + Multiply(6, 7));
     }
 
-    static int Add(int a, int b) => a + b;
-    static int Subtract(int a, int b) => a - b;
-}
+    static int Multiply(int a, int b) => a * b;
+}}
