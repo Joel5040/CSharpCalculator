24277d01ad51d9443dd3f026c42d87105e41df3f
Joel5040 <joelthompson5040>
2025-08-20 19:44:00 +0100

    feat: add subtraction function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Subtraction: 10 - 4 = " + Subtract(10, 4));
+    }
+
+    static int Subtract(int a, int b) => a - b;
+}
