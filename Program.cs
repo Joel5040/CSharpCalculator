a420c76cd541620e51cb9b6b6cf91a692c68b864
Joel5040 <joelthompson5040>
2025-08-21 11:42:07 +0100

    Merge addition feature

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ class Program
     {
         Console.WriteLine("Division: 20 / 5 = " + Divide(20, 5));
         Console.WriteLine("Subtraction: 10 - 4 = " + Subtract(10, 4));
+        Console.WriteLine("Addition: 5 + 3 = " + Add(5, 3));
     }
 
     static double Divide(double a, double b) => a / b;
     static int Subtract(int a, int b) => a - b;
+    static int Add(int a, int b) => a + b;
 }
